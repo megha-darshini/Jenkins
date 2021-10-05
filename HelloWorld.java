fb00f66d89cace6e12e44813dfd22eae6000ff7f
megha-darshini <63763292+megha-darshini@users.noreply.github.com>
2021-10-05 19:12:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----**** Hello World & Welcome to jenkins learning ****----");
+		System.out.println("-----**** Hello World & Welcome ****----");
 	}
 }
